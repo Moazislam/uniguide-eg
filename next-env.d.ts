f89cd055bc4c0c422e11fc3cdf5c75f89d9c3d22
Moaz Islam Mohasab Mahmoud <moazislam094@gmail.com>
2026-04-07 18:39:26 +0000

    fix: resolve merge conflict in next-env.d.ts

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,9 +1,6 @@
 /// <reference types="next" />
 /// <reference types="next/image-types/global" />
-<<<<<<< HEAD
-=======
 /// <reference path="./.next/types/routes.d.ts" />
->>>>>>> 6331271 (the navbar and the majors page)
 
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/app/api-reference/config/typescript for more information.
